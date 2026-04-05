5d8df9587be04b12cbd023dcef0edf38abe99f1f
parth <parthbhutada13@gmail.com>
2026-04-05 23:24:37 +0530

    Implement output generation for model response
    Add output generation and response printing to model.

--- llm_implementation.py
+++ llm_implementation.py
@@ -60,3 +60,17 @@ messages = [
 
 text = tokenizer.apply_chat_template(messages, tokenize = False, add_generation_prompt = True)
 model_inputs = tokenizer([text], return_tensors = "pt").to(model.device)
+
+# generate the output
+generated_ids = model.generate(
+    **model_inputs,
+    max_new_tokens=250,
+    temperature=0.7,
+    do_sample=True
+)
+
+generated_ids = generated_ids[:, model_inputs["input_ids"].shape[-1]:]
+
+response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+print(response)
